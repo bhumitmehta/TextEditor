a0b7571ee4b1915df15293a2775fd95c0fbc7bda
bhumitmehta <bhumitmehta2209@gmail.com>
2025-03-29 09:31:41 +0530

    Added Syntax hlighting and Fonts

--- TextEditor3.cpp
+++ TextEditor3.cpp
@@ -20,6 +20,7 @@
 #include<FL/Fl_Text_Buffer.H>
 #include<FL/Fl_Text_Display.H>
 #include<FL/Fl_Text_Editor.H>
+#include<FL/Fl_Text_Display.H>
 //include entire FLTK library in one include an one go
 using namespace std;
 
@@ -27,6 +28,7 @@ using namespace std;
 int changed = 0;
 char filename[256] = "";
 Fl_Text_Buffer *textbuf;
+Fl_Text_Buffer *stylebuf;
 char title[256] = "Untitled";
 
 
@@ -53,6 +55,12 @@ int check_save(void);
 void load_file(char*,int);
 void set_title(Fl_Window* w);
 void save_file(char *newfile);
+void replcan_cb(Fl_Widget*, void* v);
+void replall_cb(Fl_Widget*, void* v);
+void style_parse(const char*, char*, int);
+void style_update(int, int, int, int, const char*, void*);
+void changed_cb(int, int nInserted, int nDeleted,int, const char*, void* v);
+
 
 int loading = 0;
 class EditorWindow : public Fl_Double_Window {
@@ -101,7 +109,7 @@ class EditorWindow : public Fl_Double_Window {
   
     { "&Edit", 0, 0, 0, FL_SUBMENU },
       // { "&Undo",       FL_CTRL + 'z', (Fl_Callback *)undo_cb, 0, FL_MENU_DIVIDER },
-      { "Cu&t",        FL_CTRL + 'x', (Fl_Callback *)cut_cb },
+      { "&Cut",        FL_CTRL + 'x', (Fl_Callback *)cut_cb },
       { "&Copy",       FL_CTRL + 'c', (Fl_Callback *)copy_cb },
       { "&Paste",      FL_CTRL + 'v', (Fl_Callback *)paste_cb },
       { "&Delete",     0, (Fl_Callback *)delete_cb },
@@ -116,6 +124,11 @@ class EditorWindow : public Fl_Double_Window {
   
     { 0 }
   };
+
+  void undo_cb(Fl_Widget* w, void* v) {
+    EditorWindow* e = (EditorWindow*)v;
+    Fl_Text_Editor::kf_undo(0, e->editor);
+  }
 
 //   void set_title(Fl_Window* w) {
 //     if (filename[0] == '\0') strcpy(title, "Untitled");
@@ -376,6 +389,233 @@ void load_file(char *newfile, int ipos) {
   }
 
 
+  struct Style_Table_Entry {
+      Fl_Color color;
+      Fl_Font  font;
+      int      size;
+      unsigned attr;
+    };
+  
+  // Syntax highlighting arrays
+  const char *code_types[] = {
+      "auto", "bool", "char", "class", "const", "double",
+      "enum", "float", "int", "long", "short", "struct",
+      "typedef", "union", "unsigned", "void"
+  };
+  
+  const char *code_keywords[] = {
+      "break", "case", "catch", "continue", "default", "delete",
+      "do", "else", "false", "for", "if", "new", "private",
+      "protected", "public", "return", "sizeof", "switch",
+      "this", "throw", "true", "try", "while"
+  };
+  
+  // Helper function for bsearch
+  int compare_keywords(const void *a, const void *b) {
+      return strcmp(*(const char **)a, *(const char **)b);
+  }
+
+  Fl_Text_Display::Style_Table_Entry styletable[] = {     // Style table
+    { FL_BLACK,      FL_COURIER,        FL_NORMAL_SIZE }, // A - Plain
+    { FL_DARK_GREEN, FL_COURIER_ITALIC, FL_NORMAL_SIZE }, // B - Line comments
+    { FL_DARK_GREEN, FL_COURIER_ITALIC, FL_NORMAL_SIZE }, // C - Block comments
+    { FL_BLUE,       FL_COURIER,        FL_NORMAL_SIZE }, // D - Strings
+    { FL_DARK_RED,   FL_COURIER,        FL_NORMAL_SIZE }, // E - Directives
+    { FL_DARK_RED,   FL_COURIER_BOLD,   FL_NORMAL_SIZE }, // F - Types
+    { FL_BLUE,       FL_COURIER_BOLD,   FL_NORMAL_SIZE }  // G - Keywords
+  };
+  
+  void
+style_update(int        pos,          // I - Position of update
+             int        nInserted,    // I - Number of inserted chars
+             int        nDeleted,     // I - Number of deleted chars
+             int        nRestyled,    // I - Number of restyled chars
+             const char *deletedText, // I - Text that was deleted
+             void       *cbArg) {     // I - Callback data
+  int  start,                         // Start of text
+       end;                           // End of text
+  char last,                          // Last style on line
+       *style,                        // Style data
+       *text;                         // Text data
+
+
+  // If this is just a selection change, just unselect the style buffer...
+  if (nInserted == 0 && nDeleted == 0) {
+    stylebuf->unselect();
+    return;
+  }
+
+  // Track changes in the text buffer...
+  if (nInserted > 0) {
+    // Insert characters into the style buffer...
+    style = new char[nInserted + 1];
+    memset(style, 'A', nInserted);
+    style[nInserted] = '\0';
+
+    stylebuf->replace(pos, pos + nDeleted, style);
+    delete[] style;
+  } else {
+    // Just delete characters in the style buffer...
+    stylebuf->remove(pos, pos + nDeleted);
+  }
+
+  // Select the area that was just updated to avoid unnecessary
+  // callbacks...
+  stylebuf->select(pos, pos + nInserted - nDeleted);
+
+  // Re-parse the changed region; we do this by parsing from the
+  // beginning of the line of the changed region to the end of
+  // the line of the changed region...  Then we check the last
+  // style character and keep updating if we have a multi-line
+  // comment character...
+  start = textbuf->line_start(pos);
+  end   = textbuf->line_end(pos + nInserted - nDeleted);
+  text  = textbuf->text_range(start, end);
+  style = stylebuf->text_range(start, end);
+  last  = style[end - start - 1];
+
+  style_parse(text, style, end - start);
+
+  stylebuf->replace(start, end, style);
+  ((Fl_Text_Editor *)cbArg)->redisplay_range(start, end);
+
+  if (last != style[end - start - 1]) {
+    // The last character on the line changed styles, so reparse the
+    // remainder of the buffer...
+    free(text);
+    free(style);
+
+    end   = textbuf->length();
+    text  = textbuf->text_range(start, end);
+    style = stylebuf->text_range(start, end);
+
+    style_parse(text, style, end - start);
+
+    stylebuf->replace(start, end, style);
+    ((Fl_Text_Editor *)cbArg)->redisplay_range(start, end);
+  }
+
+  free(text);
+  free(style);
+}
+
+void
+style_parse(const char *text,
+            char       *style,
+            int        length) {
+  char             current;
+  int             col;
+  int             last;
+  char             buf[255],
+             *bufptr;
+  const char *temp;
+
+  for (current = *style, col = 0, last = 0; length > 0; length --, text ++) {
+    if (current == 'A') {
+      // Check for directives, comments, strings, and keywords...
+      if (col == 0 && *text == '#') {
+        // Set style to directive
+        current = 'E';
+      } else if (strncmp(text, "//", 2) == 0) {
+        current = 'B';
+      } else if (strncmp(text, "/*", 2) == 0) {
+        current = 'C';
+      } else if (strncmp(text, "\\\"", 2) == 0) {
+        // Quoted quote...
+        *style++ = current;
+        *style++ = current;
+        text ++;
+        length --;
+        col += 2;
+        continue;
+      } else if (*text == '\"') {
+        current = 'D';
+      } else if (!last && islower(*text)) {
+        // Might be a keyword...
+        for (temp = text, bufptr = buf;
+             islower(*temp) && bufptr < (buf + sizeof(buf) - 1);
+             *bufptr++ = *temp++);
+
+        if (!islower(*temp)) {
+          *bufptr = '\0';
+
+          bufptr = buf;
+
+          if (bsearch(&bufptr, code_types,
+                      sizeof(code_types) / sizeof(code_types[0]),
+                      sizeof(code_types[0]), compare_keywords)) {
+            while (text < temp) {
+              *style++ = 'F';
+              text ++;
+              length --;
+              col ++;
+            }
+
+            text --;
+            length ++;
+            last = 1;
+            continue;
+          } else if (bsearch(&bufptr, code_keywords,
+                             sizeof(code_keywords) / sizeof(code_keywords[0]),
+                             sizeof(code_keywords[0]), compare_keywords)) {
+            while (text < temp) {
+              *style++ = 'G';
+              text ++;
+              length --;
+              col ++;
+            }
+
+            text --;
+            length ++;
+            last = 1;
+            continue;
+          }
+        }
+      }
+    } else if (current == 'C' && strncmp(text, "*/", 2) == 0) {
+      // Close a C comment...
+      *style++ = current;
+      *style++ = current;
+      text ++;
+      length --;
+      current = 'A';
+      col += 2;
+      continue;
+    } else if (current == 'D') {
+      // Continuing in string...
+      if (strncmp(text, "\\\"", 2) == 0) {
+        // Quoted end quote...
+        *style++ = current;
+        *style++ = current;
+        text ++;
+        length --;
+        col += 2;
+        continue;
+      } else if (*text == '\"') {
+        // End quote...
+        *style++ = current;
+        col ++;
+        current = 'A';
+        continue;
+      }
+    }
+
+    // Copy style info...
+    if (current == 'A' && (*text == '{' || *text == '}')) *style++ = 'G';
+    else *style++ = current;
+    col ++;
+
+    last = isalnum(*text) || *text == '.';
+
+    if (*text == '\n') {
+      // Reset column and possibly reset the style
+      col = 0;
+      if (current == 'B' || current == 'E') current = 'A';
+    }
+  }
+}
+
+
 int main(){
     EditorWindow* w = new EditorWindow(640, 480, "t");
     w->editor = new Fl_Text_Editor(0, 30, 640, 370);
@@ -383,6 +623,12 @@ int main(){
     w->editor->buffer(textbuf);
     Fl_Menu_Bar *m = new Fl_Menu_Bar(0, 0, 640, 30);
     m->copy(menuitems);
+    stylebuf = new Fl_Text_Buffer();
+
+    w->editor->highlight_data(stylebuf, styletable,
+                          sizeof(styletable) / sizeof(styletable[0]),
+                          'A', NULL, 0);
+    textbuf->add_modify_callback(style_update, w->editor);
     textbuf->add_modify_callback(changed_cb, w);
     textbuf->call_modify_callbacks();
     w->editor->textfont(FL_COURIER);
